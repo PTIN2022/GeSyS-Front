8a435ec9a900451d9e7d523f6d1ef1230c71da9e
Xavier Diban <xdiban@pm.me>
2022-03-29 13:09:26 +0200

    creada página de soporte técnico y link al índice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { Select } from '@mantine/core';
 import type { NextPage } from 'next'
-import { Button } from '@mantine/core';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
@@ -10,18 +8,10 @@ const Home: NextPage = () => {
     <h1>
       Welcome to GeSyS
     </h1>
-
-    <Select
-      label="Selecciona estación"
-      placeholder="Estación"
-      data={[
-        { value: 'vg1', label: 'Vilanova i la Geltrú 1' },
-        { value: 'vg2', label: 'Vilanova i la Geltrú 2' },
-        { value: 'sg1', label: 'Sitges 1' },
-        { value: 'gf1', label: 'Garraf 1' },
-      ]}
-      onChange
-      />
+    
+    <Link href="/soporte-tecnico">
+          <a>Soporte técnico</a>
+        </Link>
     </>
   )
 }
---
+++ pages/soporte-tecnico.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Link from 'next/link';
+
+const Home: NextPage = () => {
+    return (
+        <>
+        
+        <h1>
+        Soporte técnico
+        </h1>
+        
+        <Link href="/">
+            <a>Índice</a>
+            </Link>
+        </>
+    )
+}
+
+export default Home
