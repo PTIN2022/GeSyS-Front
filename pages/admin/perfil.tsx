9a0cb734945cbb00744d9e0bfe8ec1e36e349614
alfman99 <amanresamntz@gmail.com>
2022-03-31 20:01:19 +0200

    cambiado ruta del perfil

--- pages/admin/perfil.tsx
+++ pages/admin/perfil.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link'
 
 const Perfil: NextPage = () => {
   return (
+    <>
+    <h1>Este es el perfil del trabajador actual</h1>
     <Link href="/">Home</Link>
+    </>
   )
 }
 
