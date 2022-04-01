fd775c310853482cb2bdd4b174f10d176fb8019a
alfman99 <amanresamntz@gmail.com>
2022-04-01 20:02:16 +0200

    updated mantine to last version

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
+import GeneralInfo from '../../components/GeneralInfo';
+import { Stack } from '@mantine/core'
 
 const AdminHome: NextPage = () => {
   // Dynamic import becuase of server side rendering
   const MapWithNoSSR = dynamic(() => import('../../components/MapElements/Map'), { ssr: false });
 
   return (
-    <MapWithNoSSR />
+    <Stack justify="flex-start" sx={(theme) => ({ minHeight: '100%', backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], height: 300 })}>
+      <GeneralInfo />
+      <MapWithNoSSR />
+    </Stack>
   )
 }
 
