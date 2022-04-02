0e3e0f8cee04b2f21309f730dc2297a2e1c4dbc6
edux1 <edu.pm97@gmail.com>
2022-04-02 14:18:48 +0200

    Se ha añadido el titulo "Gesys - Trabajadores" a la página(La pestaña)

--- pages/admin/trabajadores/index.tsx
+++ pages/admin/trabajadores/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 
 const ListaTrabajadores: NextPage = () => {
   return (
     <>
+    <Head>
+      <title>Gesys - Trabajadores</title>
+    </Head>
+    
     <h1>Trabajadores</h1>
     <Link href="/">Home</Link>
     </>
