bcee328a28d281daa3c458657f88d005d3f303fd
marcapde <61798173+marcapde@users.noreply.github.com>
2022-04-03 22:07:31 +0200

    promociones actualizadas

--- pages/admin/promociones/index.tsx
+++ pages/admin/promociones/index.tsx
@@ -1,67 +1,46 @@
 import { Table } from '@mantine/core';
 import { NextPage } from 'next';
-import { ActionIcon } from '@mantine/core';
-import { DotsVertical, Search } from 'tabler-icons-react';
-import { Center } from '@mantine/core';
-import { Menu } from '@mantine/core';
+import PromoRow from '../../../components/PromoRow';
+const elements = [
+    {Est: "VG1", Descuento: "30%",
+    Cupones: '50/100', Fecha_ini : "18/03/22",
+     Fecha_fin:'20/03/22'},
+    {Est: "VG2", Descuento: "15%",
+     Cupones: '27/-', Fecha_ini : "20/03/22",
+      Fecha_fin:'25/03/22'},
+    {Est: "VG1", Descuento: "50%",
+      Cupones: '2/10', Fecha_ini : "21/03/22",
+       Fecha_fin:'22/03/22'}
+  ]; 
 
 
-// AQUI HAY QUE HACER UN COMPONENTE FilaTablaPromocion
+const  Promociones: NextPage =() => {
+    return (
+      <main>
+            <head>
+                <title>Promocions</title>
+            </head> 
+              <h1>PROMOS</h1>   
+              <Table striped highlightOnHover>
+                <thead>
+                  <tr>
+                    <th>Estación</th>
+                    <th>Descuento</th>
+                    <th>Cupones Usados</th>
+                    <th>Fecha inicio</th>
+                    <th>Fecha fin</th>         
+                  </tr>       
+                </thead>
+                <tbody>
+                {elements && elements.map((element, index) => {
+                        return <PromoRow {...element}/>
+                    })}
+                </tbody>
 
-const Promociones: NextPage =() => {
-  return (
-    <>
-      <h1>Promociones</h1>   
-      <Table striped highlightOnHover>
-        <thead>
-          <tr>
-            <th>Estación</th>
-            <th>Descuento</th>
-            <th>Cupones Usados</th>
-            <th>Fecha inicio</th>
-            <th>Fecha fin</th>
-          </tr>
-        </thead>
-        
-        <tbody>
+              </Table>
 
-          <tr key = "VG1">
-            <td>VG1</td>
-            <td>30%</td>
-            <td>50/100</td>
-            <td>18/03/22</td>
-            <td>20/03/22</td>
-            <Center  style={{ width: 10, height: 40 }}>
-              <ActionIcon color="dark" radius="md">
-                <DotsVertical />
-              </ActionIcon>
-            </Center>
-          </tr>
-
-          <tr key = "VG2">
-            <td>VG2</td>
-            <td>15%</td>
-            <td>24/100</td>
-            <td>22/03/22</td>
-            <td>25/03/22</td>
-            <Menu control={
-              <Center style={{ width: 10, height: 40 }}>
-                <ActionIcon color="dark" radius="md">
-                  <DotsVertical />
-                </ActionIcon>
-              </Center>
-              }
-            >
-              <Menu.Item>Activar</Menu.Item> 
-              <Menu.Item>Desactivar</Menu.Item> 
-              <Menu.Item>Más</Menu.Item> 
-            </Menu>
-          </tr>
-
-        </tbody>
-      </Table> 
-    </>
-  )   
+    </main> 
+    
+    )   
 }
-
 export default Promociones
