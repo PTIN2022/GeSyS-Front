dd186cc6338634b182117f2e0bcc57efeccefc47
Ar1e1 <ariel.medina@estudiantat.upc.edu>
2022-04-03 15:20:51 +0200

    s

---
+++ pages/login/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+const login: NextPage = () => {
+  return (
+    <>
+    <h1>login</h1>
+    <Link href="/">Home</Link>
+    </>
+  )
+}
+
+export default login
