e005695790f8b3e0dbcb9c31c4f25df8c6f3ddfe
Ar1e1 <ariel.medina@estudiantat.upc.edu>
2022-04-03 20:10:27 +0200

    ya

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next'
-import { Button, MantineProvider, MantineTheme } from '@mantine/core'
+import { Button, MantineProvider, Input, MantineTheme, Text } from '@mantine/core'
 import Link from 'next/link'
-
+import { At, BorderRadius } from 'tabler-icons-react'
 const login: NextPage = () => {
   return (
     <>
     <h1>login</h1>
+    <Input
+      placeholder="Email"
+      style={{
+        width: "350px",
+        marginLeft: "700px",
+        BorderRadius: "10px"
+        }}/>
+    <Input
+      placeholder="Contraseña"
+      style={{
+        width: "350px",
+        marginLeft: "700px",
+        BorderRadius: "10px"
+        }}/>
     <MantineProvider theme={{
     }}
     styles={{
@@ -13,12 +27,12 @@ const login: NextPage = () => {
         root: {
           color: 'black',
           backgroundColor: theme.colors.yellow[3],
-          width: "240px",
-          marginLeft: "760px",
+          width: "350px",
+          marginLeft: "700px",
           alignItems:"center",
           borderRadius:"10px"
         }
-      }) 
+      })
     }}
     >
     <Link href="/trabajador">
@@ -29,6 +43,26 @@ const login: NextPage = () => {
       </Button>
     </Link>
     </MantineProvider>
+    <Text size='md' style={{
+      width: "350px",
+      marginLeft: "800px",
+      color: "blue"
+    }}
+    >
+        Olvidé mi contraseña?
+    
+    </Text>
+    <Text size='md' style={{
+      width: "350px",
+      marginLeft: "775px",
+      color: "black"
+    }}
+    >
+        ¿No tengo cuenta?
+        <text style={{
+          color:"blue"
+        }}> Crea una</text>
+    </Text>
     </>
   )
 }
