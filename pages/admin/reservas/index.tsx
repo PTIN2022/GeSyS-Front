1772fd346186a44d8c12d56d38acf1585eabc5c6
Marc Capde <capasc5@gmail.com>
2022-04-11 20:32:10 +0200

    basic filter added

--- pages/admin/reservas/index.tsx
+++ pages/admin/reservas/index.tsx
@@ -3,6 +3,13 @@ import Head  from 'next/head'
 import { Table } from '@mantine/core'
 import { ReservaRowProps } from '../../../interfaces';
 import ReservaRow from '../../../components/ReservaRow';
+
+
+import { forwardRef, useState } from 'react';
+import { Group, Avatar, Text, MantineColor, SelectItemProps, Autocomplete } from '@mantine/core';
+import Estacion from '../estaciones/[estacion]';
+
+
 const elements: ReservaRowProps[] = [
   {
     reservante: "antonio@gmail.com",
@@ -34,15 +41,31 @@ const elements: ReservaRowProps[] = [
 ];
 
 
+//const data = charactersList.map((item) => ({ ...item, value: item.label }));
+const data = elements.map((item) => ({ ...item, value: item.estacion }));
+
+
 
 const reservas: NextPage = () => {
+  const [value, setValue] = useState('');  
     return (
       <>
       <Head>
         <title>GeSyS - Reservas</title>
       </Head> 
-        <h1>Reservas</h1>   
-      <Table striped highlightOnHover>
+        <h1>Reservas</h1>
+    <Autocomplete
+      label="Elige la estación a filtrar"
+      placeholder="Pick one"
+      //data={data}
+      value={value} onChange={setValue} data={data}      
+      filter={(value, item) =>
+        item.value.toLowerCase().includes(value.toLowerCase().trim()) ||
+        item.reservante.toLowerCase().includes(value.toLowerCase().trim())
+      }
+    />
+    
+      <Table striped highlightOnHover >
         <thead>
           <tr>
             <th>Reservante</th>
@@ -55,8 +78,8 @@ const reservas: NextPage = () => {
           </tr>       
         </thead>
         <tbody>
-        {elements && elements.map((element, index) => {
-          return <ReservaRow key={index} {...element}/>
+        {elements && elements.filter(element => element.estacion.includes(value)).map((elementFiltrat, index )=> {
+          return <ReservaRow key={index} {...elementFiltrat}/>
         })}
         </tbody>
       </Table>
