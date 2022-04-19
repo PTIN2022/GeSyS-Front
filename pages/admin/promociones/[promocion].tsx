2ff5d9d208ed47c099a9e147369b27d76a1934b5
Marc Capde <capasc5@gmail.com>
2022-04-19 20:05:02 +0200

    added caledar

--- pages/admin/promociones/[promocion].tsx
+++ pages/admin/promociones/[promocion].tsx
@@ -2,6 +2,7 @@ import { Autocomplete, MultiSelect, Textarea } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
 import { TextInput } from '@mantine/core';
 import { Grid } from '@mantine/core';
+import { DatePicker,TimeInput } from '@mantine/dates';
 
 import { Container } from '@mantine/core';
 const data = [
@@ -12,6 +13,11 @@ const data = [
 ];
 
 const Promocion = () => {
+  const [data_ini,setData_ini] = useState<Date>(new Date());
+  const [hora_ini,setHora_ini] = useState<Date>(new Date('0:0'));
+
+  const [data_fi,setData_fi] = useState<Date>(new Date());
+  const [hora_fi,setHora_fi] = useState<Date>(new Date('12pm'));
     return(
       <>
       <h1>Promocion</h1>
@@ -37,8 +43,47 @@ const Promocion = () => {
               label="Limite Cupones"
             />
           </Grid.Col>
+
+          <br></br>
+
+          <Grid.Col span={5}>
+            <DatePicker 
+              value={data_ini}
+              onChange={setData_ini}//sale en rojo pero despues va asi que paso de ello
+              label="Fecha Inicio:"
+              required
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <TimeInput 
+              value={hora_ini}
+              onChange={setHora_ini}
+              label="Hora Inicio:"
+              
+            />
+          </Grid.Col>
+
+          <br></br>
+
+          <Grid.Col span={5}>
+            <DatePicker 
+              value={data_fi}
+              onChange={setData_fi}//sale en rojo pero despues va asi que paso de ello
+              label="Fecha Fin:"
+              required
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <TimeInput 
+              value={hora_fi}
+              onChange={setHora_fi}
+              label="Hora Fin:"
+            />
+          </Grid.Col>
         </Grid>
+
         <br></br>
+
         <Textarea readOnly={true} value={"Bueno Bonito Barato"}
             placeholder="Promoción para incentivar el uso de la estación
             en esta hora de poco uso"
