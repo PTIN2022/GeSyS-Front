5aa72e876a274ed751783f2f5fd749799a775b13
alfman99 <amanresamntz@gmail.com>
2022-04-22 21:46:11 +0200

    activado  / desactivado colores promo

--- components/PromoRow.tsx
+++ components/PromoRow.tsx
@@ -1,6 +1,6 @@
 import { PromoRowProps } from '../interfaces'
 import { Menu, Center, ActionIcon } from '@mantine/core';
-import { Adjustments, InfoSquare,  DotsVertical, Search } from 'tabler-icons-react';
+import { DotsVertical, Search } from 'tabler-icons-react';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -14,7 +14,12 @@ const PromoRow = ({ Est,Descuento, Cupones,Fecha_ini,Fecha_fin,Estado } : PromoR
             <td>{Cupones}</td>
             <td>{Fecha_ini}</td>
             <td>{Fecha_fin}</td>
-            <td>{Estado}</td>
+            <td style={Activado ? {color: 'green'} : {color: 'red'}}>{
+              Activado ? 
+              'Activado'
+               : 
+              'Desactivado'
+            }</td>
             <Menu control={
                 <Center  style={{ width: 10, height: 40 }}>
                     <ActionIcon color="dark" radius="md">
