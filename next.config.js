6d96d7a686fd68b6e252cdf5b04d6be81302b504
ShadowFurtive <silencioso4444@gmail.com>
2022-04-22 16:26:08 +0200

    22/04/2022 - Modified module

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  }
 }
 
 module.exports = nextConfig
-module.exports = {
-  experimental: {
-    outputStandalone: true,
-  },
-}
