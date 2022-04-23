4d9dff0996b50ea8cf5fbcd9644117bdb2fed249
alfman99 <43989698+alfman99@users.noreply.github.com>
2022-04-23 19:14:30 +0200

    Revert "Dev xd"

--- pages/admin/averias/index.tsx
+++ pages/admin/averias/index.tsx
@@ -9,21 +9,21 @@ const elements: AveriaRowProps[] = [
     Est: "VGA1",
     Dir: "Av. Victor Balaguer nº1",
     Date: '03/02/2022',
-    State: "Resulta",
+    State: "Resolt",
     Desc: 'El cargador en la planta 2 plaza 1 no funciona '
   },
   {
     Est: "VGA2",
     Dir: "Rambla Exposició nº22",
     Date: '20/02/2022',
-    State: 'Pendiente',
+    State: 'No Resolt',
     Desc: 'El cargador en la planta -1 plaza 3 no funciona '
   },
   {
     Est: "VGA1",
     Dir: "Av. Victor Balaguer nº1",
     Date: '30/03/2022',
-    State: 'Resuelta',
+    State: 'Resolt',
     Desc: 'El cargador en la planta 3 plaza 5 no funciona '
   }
 ];
