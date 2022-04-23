a272888298e6a60d411acb33d37dc1680170459b
Cinta <cigonci01@gmail.com>
2022-04-23 22:08:17 +0200

    Rediseny de les lletres: tamany i color + fondo blau

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from 'next';
-import { Grid, Image, Title, Space, Affix, Button } from '@mantine/core';
-import {BrandGooglePlay} from "tabler-icons-react"
+import { BackgroundImage, MantineProvider, Grid, Image, Title, Space, Affix, Button, Text } from '@mantine/core';
+import {Badge, BrandGooglePlay} from "tabler-icons-react"
 
 //import dynamic from 'next/dynamic';
 
 //const BgImage = dynamic(() => import("../pages/BGImage"), {
-  //ssr: false
+ // ssr: false
 //});
 
 const Home: NextPage = () => {
   return (  
+    <BackgroundImage
+          src="/img/blau.jpg"
+          radius="sm"
+        >
     <Grid>
+      
       <Grid.Col span={5}>
       <Image
         src={'/img/mobil.jpg'}
@@ -18,13 +23,41 @@ const Home: NextPage = () => {
         //height={height}
       />
       </Grid.Col>
-      <Grid.Col span={5} offset={1} style={{ minHeight: 120 }}>
-        <Space h={70} />
-        <Title order={1}>Carga tu vehículo eléctrico sin problemas.</Title>
+      <MantineProvider
+        theme={{
+          headings:{
+            sizes: {
+              h1: { fontSize: 55 },
+              h3: { fontSize: 30 },
+              h6: { fontSize: 12 },
+            },
+          } 
+        }}
+        
+      >
+        
+      <Grid.Col span={6} offset={0.5} style={{ minHeight: 120 }}>
+        
+        <Space h={60} />
+        <Title order={1}>
+          <Text color="#0e3bac" inherit component="span"
+             weight={700}
+          >
+            Carga tu vehículo eléctrico sin problemas.
+          </Text>
+        </Title>
         <Space h={50} />
-        <Title order={3}>Plazas de carga siempre a tu disponibilidad.</Title>
-        <Title order={3}>Reserva desde cualquier sitio.</Title>
-        <Affix position={{ bottom: 130, right: 467}}>
+        <Title order={3}> 
+          <Text color="#0e3bac" inherit component="span">
+            Plazas de carga siempre a tu disponibilidad.
+          </Text>
+        </Title>
+        <Title order={3}>
+          <Text color="#0e3bac" inherit component="span">
+            Reserva desde cualquier sitio.
+          </Text>
+        </Title>
+        <Affix position={{ bottom: 130, right: 535}}>
           <Button
             component="a"
             target="_blank"
@@ -52,10 +85,20 @@ const Home: NextPage = () => {
             Google Play
           </Button>  
         </Affix> 
-        <Space h={100} />
-        <Title order={6}>Aplicación sólo disponible para dispositivos Android</Title>   
+        <Space h={120} />
+        <Title order={6}>
+          <Text color="#0e3bac" inherit component="span">
+            Aplicación sólo disponible para dispositivos Android
+          </Text>
+        </Title> 
+        
       </Grid.Col>
+      </MantineProvider>
+      
+      
   </Grid>
+  </BackgroundImage> 
+  
   );
 }
 
