b6e9c858e25791601318842a42f18d4eee4a4333
Cinta <cigonci01@gmail.com>
2022-05-04 19:41:44 +0200

    mantineProvider borrat

--- components/Base/BaseLandingUser.tsx
+++ components/Base/BaseLandingUser.tsx
@@ -1,24 +1,11 @@
 import { AppProps } from "next/app";
-import { MantineProvider} from "@mantine/core";
 const BasicLandingUser = (props: AppProps) => {
 
   const { Component, pageProps } = props;
 
   return (
     <div>
-      <MantineProvider
-        theme={{
-          headings:{
-            sizes: {
-              h1: { fontSize: 55 },
-              h3: { fontSize: 30 },
-              h6: { fontSize: 12 },
-            },
-          } 
-        }}    
-      >
         <Component {...pageProps} />
-      </MantineProvider>
     </div>
   )
 }
