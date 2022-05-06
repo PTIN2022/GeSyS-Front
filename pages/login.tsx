5c47102242055d586b6015da41d8f32e1e38b85b
Ar1e1 <ariel.medina@estudiantat.upc.edu>
2022-05-06 22:09:11 +0200

    Login, parecido al A4, y olvidar contraseña funcional.(todo esta en el: login(pages)

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,25 @@
 import type { NextPage } from 'next'
-import { Button, MantineProvider, Input, MantineTheme, Text, AppShell, Container } from '@mantine/core'
+import { Button, MantineProvider, Input, Box, Text, Modal, Group, BackgroundImage, Container, Image, TextInput } from '@mantine/core'
 import Link from 'next/link'
-
+import { useModals } from '@mantine/modals';
+import { useForm } from '@mantine/form';
+import { useState } from 'react';
 const Login: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'email incorrecto'),
+    },
+  });
+  const [opened, setOpened] = useState(false);
   return (
     <>
-    <MantineProvider
-      styles={{
+      <MantineProvider
+        styles={{
         Button: (theme) => ({
           root: {
             color: 'black',
@@ -15,8 +28,54 @@ const Login: NextPage = () => {
           }
         })
       }}>
+      <Modal opened={opened}
+        onClose={() => setOpened(false)}
+        title="Introduzca el correo para solicitar nueva contraseña"
+      >
+        <Box mx="auto">
+          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <TextInput
+              required
+              label="Email"
+              placeholder="@email.com"
+              {...form.getInputProps('email')}
+              
+            />
 
-      <Container mt={"xl"} size={"xs"} px={"xs"}>
+            <Group position="center" mt="md">
+            <Button type="submit"  onClick={() => setOpened(false)} >Solicitar</Button>
+              <Text align='center' color={'black'} onClick={() => setOpened(false)} >
+                <Link href="login">
+                  Cancelar
+                </Link>
+              </Text>
+            </Group>
+          </form>
+        </Box>
+      </Modal>
+
+    <Box sx={{ Width:'100%' }} mx="auto">
+    <BackgroundImage
+      src="/img/fondo.jpeg"
+      radius="sm"
+      >
+
+        
+      <Container size={"xs"} px={"xs"}>
+      <div style={{ width: 200, marginLeft: 'auto', marginRight: 'auto' }}>
+      <Image
+        radius="md"
+        src={'/img/logofeo.png'}
+        alt={"Foto de la aplación móvil"}
+      />
+    </div>
+        <Text align='center' size='xl' style={{
+          color:'blue'
+          
+          }}>
+          Bienvenido a Gesys
+        </Text>
+
         <Input type={"text"} placeholder={"Email"} />
         <Input type="password" placeholder="Contraseña" mt={"xs"} />
 
@@ -26,14 +85,31 @@ const Login: NextPage = () => {
           </Button>
         </Link>
         
-        <Text size='md' style={{
-          color: "blue"
-        }}>
-          Restablecer contraseña
+        <Text align='center' color={'blue'} onClick={() => setOpened(true)} >
+          <Link href="login">
+            Olvidé mi contraseña?
+          </Link>
+        </Text>
+        <Text mt={480} align='center' color={'black'}>
+          <Link href="admin">
+            about
+          </Link>
+            |
+          <Link href="admin">
+            Network
+          </Link>
+            |
+            <Link href="admin">
+            contact
+          </Link>
         </Text>
       </Container>
+
+    </BackgroundImage>
+    </Box>
     </MantineProvider>
     </>
+    
   )
 }
 
