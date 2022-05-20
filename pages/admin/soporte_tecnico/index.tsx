35ce70bb2ef3308420458a82d4d4789ad9df9d9f
Cinta <cigonci01@gmail.com>
2022-05-20 14:16:24 +0200

    soporte tecnico OK

--- pages/admin/soporte_tecnico/index.tsx
+++ pages/admin/soporte_tecnico/index.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from 'next';
 import { Table, Space, Text, Title } from '@mantine/core';
 import FilaSoporte from '../../../components/FilaTablaSoporte';
+import { useEffect, useState } from 'react';
 
 export interface SoporteRowProps {
   Name: string;
   Problema: string;
   Date: string; 
 }
-
+/*
 const SoporteDataMock: SoporteRowProps[] = [
   {
     Name: "Cloe Bermudez",
@@ -29,9 +30,30 @@ const SoporteDataMock: SoporteRowProps[] = [
     Problema: "Problema con aplicación mobil",
     Date: "11:35"
   }
-];
+];*/
 
 const SoporteTecnico : NextPage =() => {
+  const [SoporteDataMock, setAverias] = useState<SoporteRowProps[]>();
+
+  useEffect(() => {
+    const fetchEstacion = async () => {
+      const result = await fetch('https://craaxkvm.epsevg.upc.es:23600/api/soporte');
+      const data = await result.json();  
+
+      const est = []
+
+      for(let i=0; i<data.length; i++) {
+        let est1:SoporteRowProps = {
+          Name:  data[i].ticket_id,
+          Problema:  data[i].descripcion,
+          Date:  data[i].fecha,  
+        }
+        est.push(est1)
+      }
+      setAverias(est);
+    }
+    fetchEstacion();
+  }, [])
   return (
     <>
       <Title order={1}> <Text  inherit component="span">Soporte Técnico </Text></Title>
@@ -39,7 +61,7 @@ const SoporteTecnico : NextPage =() => {
       <Table striped highlightOnHover>
         <thead>
           <tr>
-            <th>Nombre</th>
+            <th>Ticket ID</th>
             <th>Problema</th>
             <th>Fecha</th>
           </tr>
