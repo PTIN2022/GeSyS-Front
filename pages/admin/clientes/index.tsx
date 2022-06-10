2934fec9a0d4201034642ad3cecb23b0e1007d6a
Marc Capde <capasc5@gmail.com>
2022-06-10 16:36:01 +0200

    get + formato adaptado

--- pages/admin/clientes/index.tsx
+++ pages/admin/clientes/index.tsx
@@ -3,147 +3,78 @@ import Head from 'next/head';
 import { useForm, formList } from '@mantine/form';
 import { Table, useMantineTheme, Modal, TextInput, Switch, Group, ActionIcon, Box, Text, Button, Title, Space } from '@mantine/core';
 import { Trash } from 'tabler-icons-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ClientesRow from '../../../components/ClientesRow';
+
+export interface ClientesData {
+  id : number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  dni:string;
+  telefono:number;
+  username: string;
+}
+
 
 const ListaClientes: NextPage = () => {
+  
+  const [elements, setClientes] = useState<ClientesData[]>();
 
-        const form = useForm({
-            initialValues: {
-              clientes: formList([{
-                  nombre: "Manu",
-                  email: "manu@gmail.com",
-                  telefono: '+34 622533636',
-                  active: false
-                },
-                {
-                  nombre: "Gerard",
-                  email: "gerard@gmail.com",
-                  telefono: '+34 673323222',
-                  active: false
-                },
-                {
-                  nombre: "Manolo",
-                  email: "manolo@gmail.com",
-                  telefono: '+34 673273277',
-                  active: true
-                }, {
-                  nombre: "Edu",
-                  email: "Edu@gmail.com",
-                  telefono: '+34 643273277',
-                  active: 'true'
-                }
-              ])
-            }
-        });
+  useEffect(() => {
+    const fetchCliente = async () => {
+      const result = await fetch('https://craaxkvm.epsevg.upc.es:23600/api/clientes', {
+        method:'GET',
+        headers:{
+          'accept': 'application/json'
+        },
+       //mode:'no-cors'
+      });
+      const data = await result.json();  
+      console.log(data[0])
+      const clientes = []
 
-        const elements = [{
-          nombre: "Manu",
-          email: "manu@gmail.com",
-          telefono: '+34 622533636'
-        }];
-
-        const rows = elements.map((element) => (
-            <tr key={element.nombre}>
-                <td>{element.nombre}</td>
-                <td>{element.email}</td>
-                <td>{element.telefono}</td>
+      for(let i=0; i<data.length; i++) {
+        let cl1:ClientesData = {
+          id: data[i].id_usuari,
+          nombre: data[i].nombre.toString(),
+          apellido: data[i].apellido,
+          email: data[i].email,
+          dni: data[i].dni,
+          telefono: data[i].telefono,
+          username: data[i].username,
+        }
+        clientes.push(cl1)
+      }
+      {clientes.length>0 && setClientes(clientes)}
+    }
+    fetchCliente();
+  }, [])
+  return (
+    <>
+    <Title order={1}> <Text  inherit component="span">Clientes </Text></Title>
+    <Space  h={25}/>
+    <Table striped highlightOnHover>
+        <thead>
+            <tr>
+              <th>Nomre</th>
+              <th>Apellido</th>
+              <th>DNI</th>
+              <th>Email</th>
             </tr>
-          ));
+        </thead>
+        <tbody>    
+         
+            {elements && elements.map((element, index) => {
+                return <ClientesRow key={index} cliente={element}/>
+            })}  
+              
+                     
+        </tbody>
+    </Table>
 
-        const [opened, setOpened] = useState(false);
-        const theme = useMantineTheme();
+    </>
+  )
+}
 
-        const fields = form.values.clientes.map((_, index) => (
-            <Group key={index} mt="xs">
-            <TextInput
-                placeholder="Nombre"
-                required
-                sx={{ flex: 1 }}
-                {...form.getListInputProps('clientes', index, 'nombre')}
-            />
-            <TextInput
-                placeholder="email"
-                required
-                sx={{ flex: 1 }}
-                {...form.getListInputProps('clientes', index, 'email')}
-            />
-            <TextInput
-                placeholder="telefono"
-                required
-                sx={{ flex: 1 }}
-                {...form.getListInputProps('clientes', index, 'telefono')}
-            />
-            <Switch label="Active" {...form.getListInputProps('clientes', index, 'active')} />
-            <ActionIcon color="red" variant="filled" onClick={() => form.removeListItem('clientes', index)}>
-                <Trash size={16} />
-            </ActionIcon>
-            <Modal
-                opened={opened}
-                onClose={() => setOpened(false)}
-                title="Perfil Cliente"
-                overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
-                overlayOpacity={0.25}>
-                {
-                  <Group> 
-                    <Table striped highlightOnHover>
-                        <thead>
-                            <tr>
-                            <th>nombre</th>
-                            <th>Email</th>
-                            <th>telefono</th>       
-                            </tr>       
-                        </thead>
-                        <tbody>{rows}
-                        </tbody>
-                     </Table>
-                </Group>
-
-                }
-            </Modal>
-            <Button color="blue" onClick={() => setOpened(true)}>Detalles</Button>
-            </Group>
-        ));
-        
-        return (
-          <>
-            <Head>
-                <title>GeSyS - Clientes</title>
-            </Head>
-            <Box>
-              <Title order={1}> <Text  inherit component="span">Clientes </Text></Title>
-              <Space  h={25}/>
-              <Group position='center' mt="md">
-                  <Button type='submit' color="blue"  onClick={() => form.addListItem('clientes', { nombre: '',email: '',telefono: '', active: false })}>
-                  Nuevo Cliente
-                  </Button>
-              </Group>
-              {fields.length > 0 ? (
-                  <Group mb="xs">
-                  <Text weight={500} size="sm" sx={{ flex: 1 }}>
-                      Nombre
-                  </Text>
-                  <Text weight={500} size="sm" sx={{ flex: 1 }}>
-                      Email
-                  </Text>
-                  <Text weight={500} size="sm" sx={{ flex: 1 }}>
-                      telefono
-                  </Text>
-                  <Text weight={500} size="sm" pr={90}>
-                      Estado
-                  </Text>
-                  <Text weight={500} size="sm" pr={90}>
-
-                  </Text>
-                  </Group>
-              ) : (
-                  <Text color="dimmed" align="center">
-                  No hay clientes
-                  </Text>
-              )}
-              {fields}
-            </Box>
-          </>
-        );
-  }
-
-  export default ListaClientes;
+export default ListaClientes
