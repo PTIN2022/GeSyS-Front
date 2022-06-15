b7294851b67263d6483b89d041b72b816b69ce2e
Marc Capde <capasc5@gmail.com>
2022-06-15 12:32:44 +0200

    Added modal editar cliente

--- components/ClientesRow.tsx
+++ components/ClientesRow.tsx
@@ -2,13 +2,15 @@ import { Menu, Center, ActionIcon } from '@mantine/core';
 import { DotsVertical, Trash } from 'tabler-icons-react';
 import Link from 'next/link';
 import { ClientesData } from '../pages/admin/clientes';
+import EditClient from './EditCliente';
+import { useEffect, useState } from 'react';
 
 // { id ,reservante ,matricula, estacion,nPlaza, date,duration, kwh, money } : ReservaRowProps, handleDeleteClick:any
 
 
 
 const ClientesRow = (props: any) => {
-
+    const [MenuOpened,setMenu] = useState(false)
     const cliente: ClientesData = props.cliente;
     const borrarElemento: (id: number) => {} = props.deleteElement;
 
@@ -52,7 +54,13 @@ const ClientesRow = (props: any) => {
        
         
     }
-
+    // var menu = false
+    // useEffect(()=>{
+    // setMenu(menu)
+    // },[menu])
+    const tancaMenu=()=>{
+        setMenu(!MenuOpened)
+    }
     return (
         <tr>
             <td>{cliente.nombre}</td>
@@ -60,21 +68,23 @@ const ClientesRow = (props: any) => {
             <td>{cliente.dni}</td>
             <td>{cliente.email}</td>
             { <td> 
-                <Menu control={
+                <Menu  opened={MenuOpened} control={
                     <Center  style={{ width: 10, height: 40 }}>
-                        <ActionIcon color="dark" radius="md">
+                        <ActionIcon   onClick={() => setMenu(!MenuOpened)} color="dark" radius="md">
                             <DotsVertical />
                         </ActionIcon>
                     </Center>
                     }>
-                    {/*
-                    <Link href={`/admin/reservas/${reserva.id}`}  passHref={true}>
-                            <Menu.Item>Editar</Menu.Item>
-                        </Link>
-                    */}
-                    <Menu.Item color={'red'} onClick={handleDelete}icon={<Trash size={14}/>} >Eliminar</Menu.Item> 
+                    
+                    <Menu.Item >
+                        <EditClient cliente={cliente} menu={tancaMenu}/>
+                    </Menu.Item>
+                   
+                    <Menu.Item color={'red'} onClick={handleDelete} icon={<Trash size={14}/>} >Eliminar</Menu.Item> 
                 </Menu>
-            </td>       }
+            </td>     
+             
+        }
         </tr>
     );
 } 
